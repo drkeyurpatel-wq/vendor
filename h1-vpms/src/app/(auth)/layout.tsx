10911afd06d2c905a2bd0c7bdc574254faded994
drkeyurpatel-wq <drkeyurpatel@gmail.com>
2026-03-11 19:50:06 +0530

    Delete h1-vpms directory

--- h1-vpms/src/app/(auth)/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function AuthLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="min-h-screen bg-gradient-to-br from-[#1B3A6B] to-[#0D7E8A] flex items-center justify-center p-4">
-      {children}
-    </div>
-  )
-}
